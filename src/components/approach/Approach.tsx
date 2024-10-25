eacbd8d75ec04b0aed8a307a64d806b11294535a
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-25 13:24:46 +0100

    Approach.tsx - desktop done

--- src/components/approach/Approach.tsx
+++ src/components/approach/Approach.tsx
@@ -1,9 +1,48 @@
 import './approach.css';
 
-const Approach:React.FC = () =>{
-    return(
+/**
+ * Renders the Approach section, showcasing the key guidelines for building a successful brand.
+ * 
+ * @returns {JSX.Element} - The rendered Approach component
+ */
+const Approach: React.FC = () => {
+    return (
         <section id="approach">
-            
+            <h2>Our approach for creating a winning brand</h2>
+            <div id="approach-content">
+                <div id="approach-guidelines">
+                    <div className='guideline-container'>
+                        <div className='guideline-point-number'>01</div>
+                        <div className='guideline-point-content'>
+                            <p className='guideline-point-header'>Brand Strategy</p>
+                            <p className='mainText'>
+                                Brand strategy is critical for long-term success. Outshining
+                                competitors and capturing the target audience are key.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='guideline-container'>
+                        <div className='guideline-point-number'>02</div>
+                        <div className='guideline-point-content'>
+                            <p className='guideline-point-header'>Brand Design</p>
+                            <p className='mainText'>
+                                Keeping the brand design unique and meaningful helps in communicating
+                                the brand’s timeless value effectively.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='guideline-container'>
+                        <div className='guideline-point-number'>03</div>
+                        <div className='guideline-point-content'>
+                            <p className='guideline-point-header'>Web Design</p>
+                            <p className='mainText'>
+                                A beautifully crafted website is the best tool for brand awareness,
+                                and ultimately results in increased revenues.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
--- src/components/brandNaming/BrandNaming.tsx
+++ src/components/brandNaming/BrandNaming.tsx
@@ -3,7 +3,6 @@ import './brandNaming.css';
 const BrandNaming:React.FC = () =>{
     return(
         <section id="brandNaming">
-            
         </section>
     )
 }
