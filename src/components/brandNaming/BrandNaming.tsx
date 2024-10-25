57302762b29271f891e729c438d38b306b10897d
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-25 14:12:02 +0100

    BrandNaming.tsx - general layout

--- src/components/brandNaming/BrandNaming.tsx
+++ src/components/brandNaming/BrandNaming.tsx
@@ -1,8 +1,16 @@
 import './brandNaming.css';
 
-const BrandNaming:React.FC = () =>{
-    return(
+const BrandNaming: React.FC = () => {
+    return (
         <section id="brandNaming">
+            <div id="brandNaming-header">
+                <div>
+                    <h2>Brand naming & guidelines</h2>
+                    <nav id="brandNaming-navBar"></nav>
+                </div>
+            </div>
+            <div id="brandNaming-project">
+            </div>
         </section>
     )
 }
