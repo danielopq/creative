20156f209bda34eedee2afe630bca8cee0119963
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-25 01:20:09 +0100

    Design.tsx - desktop version

--- src/components/design/Design.tsx
+++ src/components/design/Design.tsx
@@ -1,9 +1,28 @@
 import './design.css';
 
-const Design:React.FC = () =>{
-    return(
+/**
+ * Renders the "Design is Strategic" section of the website.
+ * 
+ * This section includes an image, a heading, descriptive text, and a call-to-action 
+ * button for scheduling a call.
+ * 
+ * @returns {JSX.Element} The Design component.
+ */
+const Design: React.FC = () => {
+    return (
         <section id="design">
-
+            <div id="design-mainPic"></div>
+            <div id="design-content">
+                <div>
+                    <h2><span style={{ color: 'var(--red)' }}>Design</span> is strategic.</h2>
+                    <p className='mainText'>
+                        “A well-crafted design strategy consistently produces desired outcomes
+                        and brand awareness. We are firm believers that success lies in creative
+                        collaboration with our clients.”
+                    </p>
+                    <div id="design-button">Schedule a Call</div>
+                </div>
+            </div>
         </section>
     )
 }
